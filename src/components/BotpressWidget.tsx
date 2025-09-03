7ba274157afda693102480a62dca02dffa1cb804
Simon Risk <simonnrisk@gmail.com>
2025-09-03 13:28:56 -0400

    fix(bp-add-bot): fix issue with render - next js renders components server side first. because the botpress webchat importants reference the dom, when they are rendered server side, we error for an undefined document. to fix this, we dynamically import the libraries with server side rendering disabled

--- src/components/BotpressWidget.tsx
+++ src/components/BotpressWidget.tsx
@@ -1,38 +1,51 @@
 "use client";
 
 import { useState } from "react";
-import { Fab, Webchat } from "@botpress/webchat";
+
+/*these must important something that accesses the DOM. Because nextjs renders server 
+side before browser / client side, "documemt" is missing from the dom on render and the imports 
+cause a document missing error to surface
+*/
+import dynamic from "next/dynamic";
+
+const Webchat = dynamic(
+  () => import("@botpress/webchat").then((m) => m.Webchat),
+  { ssr: false }
+);
+const Fab = dynamic(() => import("@botpress/webchat").then((m) => m.Fab), {
+  ssr: false,
+});
 
 export const BotpressWidget = () => {
-    const [isWebchatOpen, setIsWebchatOpen] = useState(false)
-    const clientId = process.env.NEXT_PUBLIC_BOTPRESS_CLIENT_ID!;
-    const toggle = () => setIsWebchatOpen((v) => !v);
+  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
+  const clientId = process.env.NEXT_PUBLIC_BOTPRESS_CLIENT_ID!;
+  const toggle = () => setIsWebchatOpen((v) => !v);
 
   return (
     <>
       <Webchat
         clientId={clientId}
         style={{
-            width: '400px',
-            height: '600px',
-            display: isWebchatOpen ? 'flex' : 'none',
-            position: 'fixed',
-            bottom: '90px',
-            right: '20px',
-          }}
-        />
+          width: "400px",
+          height: "600px",
+          display: isWebchatOpen ? "flex" : "none",
+          position: "fixed",
+          bottom: "90px",
+          right: "20px",
+        }}
+      />
 
       <Fab
         onClick={toggle}
         aria-label={isWebchatOpen ? "Close chat" : "Open chat"} //for accessibility
         style={{
-            position: 'fixed',
-            bottom: '20px',
-            right: '20px',
-            width: '64px',
-            height: '64px'
-          }}
-        />
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          width: "64px",
+          height: "64px",
+        }}
+      />
     </>
   );
-}
+};
